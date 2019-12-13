0017acfbaf15fc767729d0814d890d0ae3104838
Kyle Chatman <kchatman@gmail.com>
2019-12-13 14:31:21 -0800

    functional

--- app/src/main/java/com/etoitau/pixeldungeon/ui/HealthIndicator.java
+++ app/src/main/java/com/etoitau/pixeldungeon/ui/HealthIndicator.java
@@ -1,3 +1,25 @@
+/*
+ * Pixel Dungeon Echo
+ * Copyright (C) 2019 Kyle Chatman
+ *
+ * Based on:
+ *
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 package com.etoitau.pixeldungeon.ui;
 
 import com.etoitau.pixeldungeon.actors.Actor;
@@ -67,9 +89,14 @@ public class HealthIndicator extends Component {
 
         target = ch;
 
-        timer = new HealthIndicatorTimer();
+        if (!timer.hasTime) {
+            Actor.remove(timer);
+            timer = new HealthIndicatorTimer();
+            Actor.add(timer);
+        }
+
         timer.setTimer(duration);
-        Actor.add(timer);
+
     }
 }
 
--- app/src/main/java/com/etoitau/pixeldungeon/ui/HealthIndicatorManager.java
+++ app/src/main/java/com/etoitau/pixeldungeon/ui/HealthIndicatorManager.java
@@ -1,31 +1,26 @@
 /*
- * Pixel Dungeon
- * Copyright (C) 2012-2015 Oleg Dolya
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
- */
+* Pixel Dungeon Echo
+* Copyright (C) 2019 Kyle Chatman
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
 package com.etoitau.pixeldungeon.ui;
 
-import com.watabau.gltextures.TextureCache;
-import com.watabau.noosa.Gizmo;
-import com.watabau.noosa.Image;
 import com.watabau.noosa.ui.Component;
 import com.etoitau.pixeldungeon.actors.Char;
-import com.etoitau.pixeldungeon.sprites.CharSprite;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class HealthIndicatorManager extends Component {
